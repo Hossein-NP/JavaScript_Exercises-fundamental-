033e270453db816bc013e58868a321273f09ff55
Hossein <hosseinnoruzpur@outlook.com>
2024-04-07 14:15:47 +0330

    js-ex

--- 18-Login(CaseInsensitive)/app.js
+++ 18-Login(CaseInsensitive)/app.js
@@ -6,7 +6,7 @@ let userPassword = prompt('رمز عبور خود را وارد کنید.'); //e
 if(userName.length < 3 || userPassword.length < 5) {
   alert(`نام کاربری شما یا پسورد مجاز نمی باشد.نام کاربری حداقل 3 کاراکتر و پسورد 5 کاراکتر باشد.`);
 }
-else if(userName.includes("ali") == true && userPassword.length >= 5) {
+else if(userName.toLocaleLowerCase() == 'ali' && userPassword.length >= 5) {
   alert(`شما با موفقیت وارد شدید.`);
 }
 else {
